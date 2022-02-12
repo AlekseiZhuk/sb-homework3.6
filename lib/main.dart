c1db1561d6933abe3137a42e867442e64a0538b6
Zhuk Alexey <vizuwi@mail.ru>
2022-02-12 16:12:44 +0300

    1-3 Tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/avd.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import 'CarouselDemo.dart';
 
 void main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+  runApp(CarouselDemo());
 }
 
 class MyApp extends StatelessWidget {
@@ -40,6 +43,23 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(height: 400.0),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(color: Colors.amber),
+                        child: Text(
+                          'text $i',
+                          style: TextStyle(fontSize: 16.0),
+                        ));
+                  },
+                );
+              }).toList(),
+            ),
             SvgPicture.asset('assets/images/pic.svg'),
             SvgPicture.network(
               'http://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg',
